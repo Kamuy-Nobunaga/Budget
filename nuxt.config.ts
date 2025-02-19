76051e7361d706c54db6b1ebf22aef6bbc14b421
Peter <booanchen@gmail.com>
2025-02-19 21:20:58 +0800

    nuxt.config reset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/Budget/'
+  },
+  nitro: {
+    preset: 'github_pages', 
+    prerender: {
+      routes: ['/']
+    }
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }, 
   css: ['@/assets/css/main.css'],
